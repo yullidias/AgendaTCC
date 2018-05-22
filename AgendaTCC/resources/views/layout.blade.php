9a5905258e7895bda2c46fa5d20e589c02e95198
allinesferreira <allinesantos@yahoo.com.br>
2018-05-22 18:36:22 -0300

    CSU20- Finalizado. Criação dos menus conforme permissão de usuário.

--- AgendaTCC/resources/views/layout.blade.php
+++ AgendaTCC/resources/views/layout.blade.php
@@ -9,6 +9,7 @@
 
 <body>
 
+
 	<!-- -->
 	<nav class='navbar navbar-inverse'>
 		<div class='container'>
@@ -21,9 +22,62 @@
 				<a class='navbar-brand' href='#'>AgendaTCC</a>
 			</div>
 
+
 			<div class='collapse navbar-collapse' id='barra-navegacao'>
 				<ul class='nav navbar-nav navbar-right'>
 					@if(auth()->check()){{-- se ele estiver logado --}}
+						@if(auth()->user()["professor"] == 0)
+							<div class="btn-group pull-left ">
+								<p> </p>
+								<button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" >
+									Gestão do Aluno
+								</button>
+								<div class="dropdown-menu dropdown-menu-right">
+									<li><a href="{{route('cadastrar_aluno')}}"><strong>Cadastrar</strong></a></li>
+									{{--<li><a href="{{route('aluno_visualizar_cronograma')}}"><strong>Cronograma</strong></a></li>--}}
+									{{--<li><a href="{{route('inclui a rota Notas')}}"><strong>Notas</strong></a></li>--}}
+									<li><a href="{{route('submeter_tcc')}}"><strong>TCC</strong></a></li>
+								</div>
+								<p> </p>
+							</div>
+
+						@endif
+						@if(auth()->user()["professor"] == 1)
+							<div class="btn-group pull-left ">
+								<p> </p>
+								<button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" >
+									Gestão do Professor
+								</button>
+								<div class="dropdown-menu dropdown-menu-right">
+									<li><a href="{{route('cadastrar_professor')}}"><strong>Cadastrar</strong></a></li>
+									<li><a href="{{route('professor_visualizar_cronograma')}}"><strong>Cronograma</strong></a></li>
+									{{--<li><a href="{{route('incluir rota orientandos')}}"><strong>Orientandos</strong></a></li>--}}
+									{{--<li><a href="{{route('incluir rota relatorio')}}"><strong>Relatório</strong></a></li>--}}
+									<li><a href="{{route('visualizar_lista_alunos')}}"><strong>Turma</strong></a></li>
+								</div>
+
+							</div>
+						@endif
+						@if(auth()->user()["professor"] == 1 and auth()->user()["orientador"]==1)
+							<li><a href="{{route('salvar_agendamento')}}"><strong>Agendamento</strong></a></li>
+						@endif
+						@if(auth()->user()["professor"] == 1 and auth()->user()["gestor"]==1)
+
+							<div class="btn-group pull-left ">
+								<p> </p>
+								<button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" >
+									Gerir Sistema
+								</button>
+								<div class="dropdown-menu dropdown-menu-right">
+									<li><a href="{{route('listar_alunos')}}"><strong>Listar Alunos</strong></a></li>
+									<li><a href="{{route('listar_professores')}}"><strong>Listar Professores</strong></a></li>
+									<li><a href="{{route('listar_atividades_cronograma')}}"><strong>Gerir Cronograma</strong></a></li>
+									<li><a href="{{route('listar_salas')}}"><strong>Gerir Salas</strong></a></li>
+									<li><a href="{{route('gerir_semestres')}}"><strong>Gerir Semestres</strong></a></li>
+									{{--<li><a href="{{route('incluir a rota do relatorio')}}"><strong>Gerir Relatório</strong></a></li>--}}
+								</div>
+							</div>
+						@endif
 						<li><a href="#">{{auth()->user()->nome}}</a></li> {{-- mostro o nome de quem está logado --}}
 						<li><a href="{{route('site.login.sair')}}"><strong>Sair</strong></a></li> {{-- sai do login --}}
 						@endif
